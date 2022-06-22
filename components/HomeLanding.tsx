b1a94b1cbb2c1bf99288ad249160eccd8312c148
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2022-06-22 18:07:44 -0400

    feat: Favicon, SVG Shape Component, & Subheading

--- components/HomeLanding.tsx
+++ components/HomeLanding.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styles from "../styles/HomeLanding.module.scss";
+import SVGShape from "./SVGShape";
 
 function HomeLanding() {
   return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.heading}>Borhan Saflo</h1>
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        <h1 className={styles.heading}>Hi, I'm Borhan Saflo</h1>
+        <h2 className={styles.subHeading}>A Web Developer</h2>
+      </div>
+      <SVGShape />
+    </>
   );
 }
 
