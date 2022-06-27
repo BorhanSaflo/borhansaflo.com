268f56fb2cb33dd1073003bc41e88be5bf5ebf00
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2022-06-27 00:40:40 -0400

    feat: Section Component Pt.1

--- components/Section.tsx
+++ components/Section.tsx
@@ -2,7 +2,28 @@ import React from "react";
 import styles from "../styles/Section.module.scss";
 
 function Section() {
-  return <div className={styles.wrapper}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h2 className={styles.heading}>Heading</h2>
+        <p className={styles.paragraph}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Section;
