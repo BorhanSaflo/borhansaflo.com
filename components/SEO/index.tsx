bb5e83b9a7bd1bb0dcb4e31e2704e890da96d60d
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2022-07-31 23:54:59 -0400

    feat: Google Analytics

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -55,6 +55,22 @@ const SEO = ({ seo }: { seo: SEO }) => {
       {socialTags(seo).map(({ name, content }) => {
         return <meta key={name} name={name} content={content} />;
       })}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </Head>
   );
 };
