110f0bd100e1375ba55ee0d1eb4b0cfc1e31d894
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-05-03 19:59:05 -0400

    feat: Sanity Private Datasets

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -2,6 +2,7 @@ const config = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  token: process.env.SANITY_API_TOKEN,
   useCdn: true,
 };
 
