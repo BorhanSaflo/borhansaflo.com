6e6c3f3fce61a22d04ec9a947c5ccfca15865305
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-11-21 02:15:58 -0500

    fix: Next.js 14 Remote Patterns
    - Use the Next.js remote patterns property in the next config instead of the deprecated domains property.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.sanity.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "**",
+      }
+    ]
   },
   async rewrites() {
     return [
