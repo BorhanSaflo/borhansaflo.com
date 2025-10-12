1d1aa03055cc0af23a49aa9f607d13d867b98d4c
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2025-10-12 13:16:23 -0400

    chore: Set Sanity Client Client Perspective to Published

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
+import { StructureBuilder, StructureResolverContext, structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { vercelDeployTool } from "sanity-plugin-vercel-deploy";
 import { crossDatasetDuplicator } from "@sanity/cross-dataset-duplicator";
@@ -9,14 +9,14 @@ import { deskStructure } from "./sanity/deskStructure";
 const studioConfig = defineConfig([
   {
     name: "production",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     dataset: "production",
-    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION as string,
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
     title: "borhansaflo.com Production Studio",
     basePath: "/admin/production",
     plugins: [
       structureTool({
-        structure: (S: any, context: any) => {
+        structure: (S: StructureBuilder, context: StructureResolverContext) => {
           return deskStructure(S, context);
         },
       }),
@@ -44,14 +44,14 @@ const studioConfig = defineConfig([
   },
   {
     name: "staging",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     dataset: "staging",
-    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION as string,
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
     title: "borhansaflo.com Staging Studio",
     basePath: "/admin/staging",
     plugins: [
       structureTool({
-        structure: (S: any, context: any) => {
+        structure: (S: StructureBuilder, context: StructureResolverContext) => {
           return deskStructure(S, context);
         },
       }),
